7bd2689c1fe0c8967a23af2ecf8fbd4f0fb1576e
carl <1516525165@qq.com>
2020-02-28 16:20:23 +0800

    first

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {products} from '../products'
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+  products = products;
+
+  share() {
+    window.alert("the product has been shared");
+  }
+  onNotify() {
+    window.alert("You will be notified when the product goes on sale");
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
